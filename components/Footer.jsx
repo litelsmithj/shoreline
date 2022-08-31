f5e33d90e847cdeeafed2d69b8bfec3735edba70
Jake Litel-Smith <litelsmithj@gmail.com>
2022-08-31 08:14:19 -0700

    Add Layout structure with NavBar and Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
 
 function Footer() {
   return (
-    <div>
-      Footer
+    <div className='footer-container'>
+      <p>2022 JLS Ecommerce All rights reserved</p>
+      <p className='icons'>
+        <AiFillInstagram/>
+        <AiOutlineTwitter/>
+      </p>
     </div>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import Layout from '../components/Layout'
+import {Toaster} from 'react-hot-toast'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Toaster/>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
